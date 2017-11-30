3fe17ecb253eeb6df1ad46e0115efd0fa955bcfd
Fabian Kainz <fabian.e.kainz@gmail.com>
2017-11-30 13:57:47 +0100

    file cleanup

--- NodeJS/code.js
+++
@@ -1,4 +0,0 @@
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
